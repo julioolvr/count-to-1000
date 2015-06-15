4b01651618e0b8d251125fddbb5d6724bc46f2f8
Sergio Rafael Gianazza <sgianazza@gmail.com>
2015-06-15 16:52:55 -0300

    Fetch ws url from rtm.start

--- run.js
+++ run.js
@@ -1,24 +1,38 @@
 var WebSocket = require('ws'),
-    url = 'wss://', // Slack RTM endpoint
-    ws = new WebSocket(url),
+    apiToken = "", //Api Token from https://api.slack.com/web (Authentication section)
+    authUrl = "https://slack.com/api/rtm.start?token=" + apiToken,
+    request = require("request"),
     userId = 'U0375BYGC', // Id for the user the bot is posting as
     channelId = 'G069T2UV9'; // Id of the channel the bot is posting to
 
-ws.on('open', function() {
-    console.log('Connected');
-});
-
-ws.on('message', function(message) {
-    console.log('received:', message);
-    message = JSON.parse(message);
-
-    if (message.channel === channelId && message.type === 'message' && message.user !== userId && message.text.match(/^\d+$/)) {
-        var number = Number(message.text);
-
-        if (number) {
-            var next = number + 1;
-            console.log('received number:', number, 'sending:', next);
-            ws.send(JSON.stringify({ channel: channelId, id: 1, text: String(next), type: "message" }));
-        }
+request(authUrl, function(err, response, body) {
+  if (!err && response.statusCode === 200) {
+    var res = JSON.parse(body);
+    if (res.ok) {
+      connectWebSocket(res.url);
     }
+  }
 });
+
+function connectWebSocket(url) {
+  var ws = new WebSocket(url);
+
+  ws.on('open', function() {
+      console.log('Connected');
+  });
+
+  ws.on('message', function(message) {
+      console.log('received:', message);
+      message = JSON.parse(message);
+
+      if (message.channel === channelId && message.type === 'message' && message.user !== userId && message.text.match(/^\d+$/)) {
+          var number = Number(message.text);
+
+          if (number) {
+              var next = number + 1;
+              console.log('received number:', number, 'sending:', next);
+              ws.send(JSON.stringify({ channel: channelId, id: 1, text: String(next), type: "message" }));
+          }
+      }
+  });
+}
